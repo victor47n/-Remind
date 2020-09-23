2004e5a23380a5422889b31d28610f0094acdd04
Jairo Doni <jairo.doni97@gmail.com>
2020-09-23 17:11:07 -0300

    add config cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes')
 const { errors } = require('celebrate');
 
@@ -7,7 +8,7 @@ const app = express();
 
 require('dotenv/config');
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
