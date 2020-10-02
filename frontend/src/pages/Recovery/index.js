6058271ca3b1e507ba5a99a6d5a8cb5fd4192bfb
Guilherme Machado Oliveira <Guimo.cb@gmail.com>
2020-10-01 23:35:03 -0300

    feature/Tela Web de Recuperar Senha Resposiva

--- frontend/src/pages/Recovery/index.js
+++ frontend/src/pages/Recovery/index.js
@@ -1,56 +1,62 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import api from '../../sevices/api';
-import './styles.css';
 
+import './styles.css';
+import lostMan from '../../assets/lost.png'
 
 export default function Recovery({ match }) {
-const [password, setPassword] = useState('');
-const [passwordConf, setPasswordConf] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConf, setPasswordConf] = useState('');
 
-function handleConfirmation(e) {
-    
-    e.preventDefault();
-    if(password === passwordConf){
-      const data = {
-        email: match.params.email,
-        token: match.params.token,
-        password
-    };  
+    function handleConfirmation(e) {
 
-    try {
-        const response = api.post('/reset_password', data)
-    alert("Senha trocada");
+        e.preventDefault();
+        if (password === passwordConf) {
+            const data = {
+                email: match.params.email,
+                token: match.params.token,
+                password
+            };
 
-    } catch (error) {
-        alert("Erro da api");
+            try {
+                const response = api.post('/reset_password', data)
+                alert("Senha trocada");
+
+            } catch (error) {
+                alert("Erro da api");
+            }
+
+        } else {
+            alert("Senhas diferentes");
+        }
+
+
     }
-    
-    } else {
-        alert("Senhas diferentes");
-    }
-    
-    
-}
 
-    return(
+    return (
         <div className="app">
-            
-                
-        <div className="container">
-            
-    
-        <form onSubmit={handleConfirmation}>
-            <div>
-            <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha" type="password"/>
-            <input value={passwordConf} onChange={e => setPasswordConf(e.target.value)}  placeholder="ConfirmarSenha" type="password"/>
+
+
+            <div className="container">
+                <div className="img">
+                    <img src={lostMan} alt="LostMan" />
+                </div>
+
+                <div className="form">
+
+                    <p>Resetar Senha</p>
+
+                    <form onSubmit={handleConfirmation}>
+                        <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha" type="password" />
+                        <input value={passwordConf} onChange={e => setPasswordConf(e.target.value)} placeholder="Confirmar Senha" type="password" />
+
+
+                        <button className="button" type="submit">RESETAR SENHA</button>
+                    </form>
+
+                </div>
             </div>
 
-            <button className="button" type="submit">CONFIRMAR</button>
-        </form>
-
-        </div>
-
-        
 
         </div>
     );
