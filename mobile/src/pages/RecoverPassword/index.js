404b562fc6bbe75f604df642174139a7f7a8ddb9
victor47n <victor47n@gmail.com>
2020-10-09 23:33:40 -0300

    recover password com desing arrumado e agr adicionadno alertas

--- mobile/src/pages/RecoverPassword/index.js
+++ mobile/src/pages/RecoverPassword/index.js
@@ -1,69 +1,98 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { MaterialIcons } from '@expo/vector-icons';
 import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import api from '../../services/api';
 
 export default function RecuperarSenha() {
     const [email, setEmail] = useState('');
 
-    async function handleRecoveryToken() {     
+    async function handleRecoveryToken() {
         const data = {
-            
+
             email,
         }
         try {
-        const response = await api.post('/forgot_password', data);
-        navigateToResetPassword();
+            const response = await api.post('/forgot_password', data);
+            Alert.alert(
+                'Alert Title',
+                'My Alert Msg',
+                [
+                    { text: 'OK', onPress: () => navigateToResetPassword() }
+                ],
+                { cancelable: false }
+            );
+
         } catch (error) {
-            alert(error);
+            Alert.alert(
+                'Aconteceu um erro!',
+                'error',
+                [
+                    { text: 'OK', onPress: () => navigateToResetPassword() }
+                ],
+                { cancelable: false }
+            );
+            // alert(error);
         }
     }
 
     const navigation = useNavigation();
 
-    function navigateBack(){
+    function navigateBack() {
         navigation.goBack();
     }
-    function navigateToResetPassword(){
+    function navigateToResetPassword() {
         navigation.navigate('ResetPassword');
     }
-    
-    return <View style={styles.background}>
-           <LinearGradient style={styles.header}
-            colors={['#6C64FB', '#9B67FF']}
-            start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}
-        >
-          
-        </LinearGradient>
-        <TouchableOpacity style={styles.buttonBack} onPress={navigateBack}>
-                    <MaterialIcons name="arrow-back" size={24} color="#000000" />
-                </TouchableOpacity>
-                
-        <View style={styles.head}>
-            <Text style={styles.titulo} >Recuperar Senha</Text>
 
-            <Text style={styles.descricao} >Insira o email de sua conta e enviaremos um token para redefinição de senha</Text>
-        </View>
-
-        <View style={styles.body}>
-
-            <TextInput style={styles.input} value={email} onChangeText={setEmail} autoCapitalize="none" placeholderTextColor="#E0E0E0" placeholder="Email" autoCorrect={false}></TextInput>
-
-            <TouchableOpacity  onPress={handleRecoveryToken} >
-            <LinearGradient style={styles.botao}
-                colors={['#6C64FB', '#9B67FF']}
-                start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}
+    return (
+        <View style={styles.background}>
+            <LinearGradient
+                colors={["#6C64FB", "#9B67FF"]}
+                style={styles.statusBar}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
             >
-                <Text style={styles.botaoTexto} >ENVIAR</Text>
-</LinearGradient>
+                <StatusBar
+                    translucent={true}
+                    backgroundColor={"transparent"}
+                    style="light"
+                />
+            </LinearGradient>
+
+            <TouchableOpacity style={styles.buttonBack} onPress={navigateBack}>
+                <MaterialIcons name="arrow-back" size={24} color="#000000" />
             </TouchableOpacity>
 
+            <View style={styles.container}>
+
+                <View style={styles.head}>
+                    <Text style={styles.title} >Recuperar Senha</Text>
+
+                    <Text style={styles.description} >Insira o email de sua conta e enviaremos um token para redefinição de senha</Text>
+                </View>
+
+
+                <TextInput style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                    placeholderTextColor="#E0E0E0"
+                    placeholder="Email"
+                    autoCorrect={false} />
+
+                <TouchableOpacity onPress={handleRecoveryToken} >
+                    <LinearGradient style={styles.button}
+                        colors={['#6C64FB', '#9B67FF']}
+                        start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}
+                    >
+                        <Text style={styles.buttonText}>ENVIAR</Text>
+                    </LinearGradient>
+                </TouchableOpacity>
+            </View>
         </View>
-
-
-
-    </View>
+    )
 }
--- mobile/src/pages/RecoverPassword/styles.js
+++ mobile/src/pages/RecoverPassword/styles.js
@@ -5,38 +5,48 @@ export default StyleSheet.create({
     background: {
         flex: 1,
         alignContent: 'center',
-        backgroundColor: '#FAFAFA'
+        backgroundColor: '#FAFAFA',
     },
-    header: {
+
+    statusBar: {
         backgroundColor: '#6C64FB',
         paddingHorizontal: 16,
-        paddingTop: Constants.statusBarHeight + 16,
-        paddingBottom: 16,
-        flexDirection: 'row',
-        alignItems: 'center',
+        height: Constants.statusBarHeight
     },
+
+    container: {
+        flex: 1,
+        alignSelf: 'center',
+        paddingHorizontal: 40,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderStyle: "solid",
+    },
+
     buttonBack: {
-        padding: 12,
+        justifyContent: "center",
+        alignItems: "center",
         width: 48,
-        height: 48
+        height: 48,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderRadius: 25,
     },
+
     head: {
         marginTop: 8,
-        paddingLeft: 36,
-        paddingRight: 44
+        marginBottom: 56,
     },
-    titulo: {
-        letterSpacing: 0.25,
-        lineHeight: 40,
-        
-        lineHeight: 40,
-        display: 'flex',
-        alignItems: 'flex-end',
-        fontSize: 34,
+
+    title: {
         fontFamily: 'Roboto',
-        margin: 0
+        fontSize: 34,
+        letterSpacing: 0.25,
+        width: '60%',
     },
-    descricao: {
+
+    description: {
         display: 'flex',
         color: '#636363',
         fontFamily: 'Roboto',
@@ -45,48 +55,43 @@ export default StyleSheet.create({
         lineHeight: 19,
         fontSize: 16
     },
-    body: {
-        marginHorizontal: 40,
-        marginTop: 56,
-        alignSelf: 'center'
-    },
+
     input: {
         shadowOffset: { width: 0, height: 3, },
         shadowColor: 'black',
         shadowOpacity: 0.25,
         shadowRadius: 8,
-        elevation: 13, 
+        elevation: 13,
         paddingLeft: 28,
-        backgroundColor: '#F9F9F9',
-        borderStyle: 'solid',
-        borderColor: '#E0E0E0',
+        backgroundColor: '#FAFAFA',
         fontFamily: 'Roboto',
-        borderWidth: 1,
         borderRadius: 25,
         height: 48,
-        width: 280,
         marginBottom: 40,
         fontSize: 16
     },
-    botao: {
+
+    button: {
+        display: 'flex',
+        justifyContent: 'center',
         backgroundColor: '#9B67FF',
         borderRadius: 25,
         alignSelf: 'center',
         elevation: 13,
         alignItems: 'center',
-        width: 280,
+        width: '100%',
         height: 36,
-        display: 'flex',
         shadowOffset: { width: 0, height: 3, },
         shadowColor: 'black',
         shadowOpacity: 0.25,
         shadowRadius: 8,
     },
-    botaoTexto: {
-        paddingVertical: 10,
+
+    buttonText: {
+        textAlignVertical: 'center',
         fontFamily: 'Roboto',
-        letterSpacing: 0.125,
-        lineHeight: 16,
+        fontSize: 14,
+        letterSpacing: 0.0125,
         fontWeight: '500',
         color: '#F5F5F5'
     }
