064b3affdda515c2c8f14468eb8885321394e74b
Guilherme Machado Oliveira <Guimo.cb@gmail.com>
2020-11-12 21:22:22 -0300

    Drawer

--- mobile/src/pages/Navigation/DrawerContent.js
+++ mobile/src/pages/Navigation/DrawerContent.js
@@ -16,7 +16,9 @@ export function DrawerContent({ route,navigation }) {
     function navigateToProfile() {
         navigation.navigate('Profile');
     }
-    
+    function navigateToSharedScreen() {
+        navigation.navigate('SharedAcc');
+    }
     
     return (
         <View style={{ flex: 1 }}>
@@ -31,6 +33,10 @@ export function DrawerContent({ route,navigation }) {
                     <TouchableOpacity style={styles.options} onPress={navigateToProfile} >
                         <MaterialIcons name="perm-identity" size={24} color="#FAFAFA" />
                         <Text style={styles.optionsText}>Perfil</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.options} onPress={navigateToSharedScreen} >
+                        <MaterialIcons name="people-outline" size={24} color="#FAFAFA" />
+                        <Text style={styles.optionsText}>Contas vinculadas</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.options} onPress={navigateToLogin}>
                         <MaterialIcons name="power-settings-new" size={24} color="#FAFAFA" />
